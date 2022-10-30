308d3b02bc5d9777898c34bc7023f63b18ba7ed1
Takoulinos <theo100theo@yahoo.gr>
2022-10-30 21:52:26 +0200

    Game now doesn't start immediately but after you have entered names and pressed confirm. Starting player is chosen randomly.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 //An Array containing the content of the gameboard
-const gameBoard = (() => {
+const gameMemory = (() => {
     const board = ['','','','','','','','','']
     const squares = document.querySelectorAll('.square');
     return {board, squares};
@@ -16,7 +16,7 @@ const player = (name, symbol) => {
 const renderStuff = (() => {
     const renderBoard = () => {
         for (let i=0; i<9; i++) {
-            gameBoard.squares[i].textContent = gameBoard.board[i];
+            gameMemory.squares[i].textContent = gameMemory.board[i];
         }
     }
     const renderMessage = message => {
@@ -26,33 +26,47 @@ const renderStuff = (() => {
     return {renderBoard, renderMessage}
 })();
 
-const player1 = player('Theo', 'X');
-const player2 = player('Kat', 'O')
+    //let player1 = player('Theo', 'X');
+    //let player2 = player('Kat', 'O');
 
 //the logic of the game
 const playGame = (() => {
-    let currentPlayer = player1;
-    message = `It's ${currentPlayer.getName()}'s turn`;
+    let gameStatus = false;
+    let player1;
+    let player2;
+    let currentPlayer;
+    message = `Press the button to begin a game`;
     renderStuff.renderMessage(message);
 
+    const beginGame = (xplayer, oplayer) => {
+        gameStatus = true;
+        player1 = xplayer;
+        player2 = oplayer;
+        currentPlayer = Math.random() < 0.5 ? player1 : player2;
+        message = `It's ${currentPlayer.getName()}'s turn`;
+        renderStuff.renderMessage(message);
+    }
+
     const playRound = (square) => {
-        if (gameBoard.board[square] === '') {
-            gameBoard.board[square] = currentPlayer.getSymbol();
-            renderStuff.renderBoard();
-            if (checkWinner(square)) {
-                message = `${currentPlayer.getName()} wins!`;
+        if (gameStatus === false || gameMemory.board[square] !== '') {
+            return
+        }
+        gameMemory.board[square] = currentPlayer.getSymbol();
+        renderStuff.renderBoard();
+        if (checkWinner(square)) {
+            message = `${currentPlayer.getName()} wins!`;
+            gameStatus = false;
+        }
+        else {
+            if (gameMemory.board.includes('')) {
+                (currentPlayer === player1) ? currentPlayer = player2 : currentPlayer = player1;
+                message = `It's ${currentPlayer.getName()}'s turn`;
             }
             else {
-                if (gameBoard.board.includes('')) {
-                    (currentPlayer === player1) ? currentPlayer = player2 : currentPlayer = player1;
-                    message = `It's ${currentPlayer.getName()}'s turn`;
-                }
-                else {
-                    message = 'Game over. It\'s a tie'
-                }
+                message = 'Game over. It\'s a tie'
             }
-            renderStuff.renderMessage(message);
-        } 
+        }
+        renderStuff.renderMessage(message);
     }
 
     const checkWinner = (index) => {
@@ -67,24 +81,37 @@ const playGame = (() => {
             [2,4,6]
         ]
 
-        return winningCombinations.filter(combination => combination.includes(index)).some(combination => combination.every(index => gameBoard.board[index] === currentPlayer.getSymbol()))
+        return winningCombinations.filter(combination => combination.includes(index)).some(combination => combination.every(index => gameMemory.board[index] === currentPlayer.getSymbol()))
     }
 
-    return {playRound}
+    return {beginGame, playRound}
 })();
 
 //Module containing the listeners for the boxes and buttons.
 const eventsListener = (() => {
-    const squares = gameBoard.squares;
+    const squares = gameMemory.squares;
     const startButton = document.querySelector('.start');
+    const confirmButon = document.querySelector('.confirm')
+    const cancelButton = document.querySelector('.cancel');
 
     squares.forEach((square) =>
         square.addEventListener('click', (e) => {
             playGame.playRound(parseInt(e.target.dataset.index));
         }));
 
-    startButton.addEventListener('click', (e) => {
-        console.log('qwer');
+    startButton.addEventListener('click', () => {
+        document.querySelector('.popup-form').style.display = 'block';
+    })
+
+    confirmButon.addEventListener('click', () => {
+        let xplayer = player('asdf', 'X');
+        let oplayer = player('qwer', 'O');
+        playGame.beginGame(xplayer, oplayer);
+        document.querySelector('.popup-form').style.display = 'none';
+    })
+
+    cancelButton.addEventListener('click', () => {
+        document.querySelector('.popup-form').style.display = 'none';
     })
 })();
 
